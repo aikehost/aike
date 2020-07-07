ce151e2aeb4f60e5ce9e653e1150e3018045ff41
aikehost <67904223+aikehost@users.noreply.github.com>
2020-07-07 09:14:27 +0800

    Update up.php

--- up.php
+++ up.php
@@ -13,10 +13,10 @@ URL https://github.com/yumusb/autoPicCdn
 error_reporting(0);
 header('Content-Type: text/html; charset=UTF-8');
 date_default_timezone_set("PRC");
-define("REPO","testforapi");//必须是下面用户名下的公开仓库
-define("USER","yumusb");//必须是当前GitHub用户名
+define("REPO","aike.github.io");//必须是下面用户名下的公开仓库
+define("USER","aikehost");//必须是当前GitHub用户名
 define("MAIL","yumusb@foxmail.com");//
-define("TOKEN","YourToken");//https://github.com/settings/tokens 去这个页面生成一个有写权限的token（repo：Full control of private repositories 和write:packages前打勾）
+define("TOKEN","cf6a4fd661f0d5c52b6aced64ce2a5600f96a399");//https://github.com/settings/tokens 去这个页面生成一个有写权限的token（repo：Full control of private repositories 和write:packages前打勾）
 
 //数据库配置文件
 $database = array(
